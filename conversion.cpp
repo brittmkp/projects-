7cd3f0f5b355de90269c45559b1073522f710b92
brittmkp <116769766+brittmkp@users.noreply.github.com>
2023-04-05 23:35:57 -0500

    mini currency converter

---
+++ conversion.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+
+int d_peso(int pesos){
+  return pesos * 0.00021810;
+}
+int d_reais(int reais){
+  return reais * 0.1988071571;
+}
+int d_soles(int soles){
+  return soles * 0.2652519894;
+}
---
+++ conversion.hpp
@@ -0,0 +1,6 @@
+#include <iostream>
+using namespace std;
+
+int d_peso(int pesos);
+int d_reais(int reais);
+int d_soles(int soles);
